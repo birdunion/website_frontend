bc3ffa4cef631dfac0082890dd53547aeaebdb70
birdunion <birdunionorg@proton.me>
2026-09-17 10:10:38 -0600

    Updated STRAPI_URL

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -17,7 +17,7 @@ export function meta({}: Route.MetaArgs) {
 
 export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: Post[] }> {
   const STRAPI_URL = import.meta.env.VITE_STRAPI_URL || 'https://website-backend-c20g.onrender.com';
-  const BLOG_URL = `${STRAPI_URL.replace(/\/$/, '')} /api/blogs?populate=image&sort=date:desc`
+  const BLOG_URL = `${STRAPI_URL.replace(/\/$/, '')}/api/blogs?populate=image&sort=date:desc`
   const res = await fetch(BLOG_URL);
 
   if (!res.ok) throw new Error("Failed to fetch data");
